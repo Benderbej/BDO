376deadf0453e68249f3924fb9ce3abd7f15996e
TemplaRus <RusTemplaR@yandex.com>
2015-11-05 19:02:06 +0300

    my changes last

--- src/java/ServletPack/TESTCLASS.java
+++ src/java/ServletPack/TESTCLASS.java
@@ -2,16 +2,12 @@ package ServletPack;
 
 public class TESTCLASS {
 
-    
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         System.err.println("OLOLO");
         System.out.println("TROLOLO");
-                System.out.println("TROLOLO");
-
-                        System.out.println("TROLOLO");
-
-                                System.out.println("TROLOLO");
+        System.out.println("TROLOLO");
+        System.out.println("TROLOLO");
+        System.out.println("TROLOLO");
 
     }
 }
