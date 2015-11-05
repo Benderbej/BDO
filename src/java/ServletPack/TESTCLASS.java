dca4bd57800629c99f918c34aa47dac1b45890ce
Benderbej <strashevskyseo@gmail.com>
2015-11-05 18:55:09 +0300

    trololo зафиксено

--- src/java/ServletPack/TESTCLASS.java
+++ src/java/ServletPack/TESTCLASS.java
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-package ServletPack;
-
-public class TESTCLASS {
-
-    
-    public static void main(String[] args)
-    {
-        System.err.println("OLOLO");
-    }
-}
-=======
 package ServletPack;
 
 public class TESTCLASS {
@@ -21,4 +9,3 @@ public class TESTCLASS {
         System.out.println("TROLOLO");
     }
 }
->>>>>>> origin/master
